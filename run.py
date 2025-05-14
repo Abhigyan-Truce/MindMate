d792428b70f462a032404d3bf494e4130ad873d2
Abhigyan-Truce <abhigyan@truce.io>
2025-05-14 04:51:35 +0000

    updated

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 """
 Script to run the application.
 """
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8001, reload=True)
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=True)
